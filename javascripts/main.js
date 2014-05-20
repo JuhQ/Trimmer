e63be010955ddc4a45b31164283aadec3634fe69
Jon LaBelle <jon@tech0.com>
2014-05-19 23:26:55 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
-console.log('This would be the main JS file.');
+(function () {
+
+  //
+  // hide header and footer
+  //
+
+  var footer = document.getElementsByTagName('footer');
+  if (footer.length) {
+    footer[0].style.visibility = 'hidden';
+  }
+
+  var header = document.getElementsByTagName('header');
+  if (header.length) {
+    var maintained = header[0].lastElementChild;
+    if (maintained) {
+      maintained.style.visibility = 'hidden';
+    }
+  }
+
+}());
